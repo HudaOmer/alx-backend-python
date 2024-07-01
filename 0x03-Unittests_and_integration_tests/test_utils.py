90e1001c708c7d91edb0abeedb7861fda1bb3d9a
HudaOmer <hudaoe175@gmail.com>
2024-07-01 22:38:09 +0000

    Solve task 0

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+0. Parameterize a unit test
+"""
+import unittest
+from typing import Dict, Tuple, Union
+from unittest.mock import patch, Mock
+from parameterized import parameterized
+
+from utils import (
+    access_nested_map,
+    get_json,
+    memoize,
+)
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    A class that inherits from unittest.TestCase
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            expected: Union[Dict, int],
+            ) -> None:
+        """
+         unit test for utils.access_nested_map
+        """
+        result = get_value_from_nested_map(nested_map, path)
+        self.assertEqual(result, expected_result)
+
+
